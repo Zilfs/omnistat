012bdb72e698e31f1f553241fcca4e9247af55ef
Zilfs <rheinlandcast@gmail.com>
2026-06-23 08:33:30 +0700

    feat [memory]: added simple memory tracking by printing memory usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{thread::sleep, time::Duration};
+
+use sysinfo::System;
+
 fn main() {
-    println!("Hello, world!");
+    let mut system = System::new();
+
+    loop {
+        system.refresh_all();
+
+        let total_memory = system.total_memory() / 1024 / 1024;
+        let used_memory = system.used_memory() / 1024 / 1024;
+
+        println!("Memory usage: {} MB / {} MB", used_memory, total_memory);
+
+        sleep(Duration::from_secs(3));
+    }
 }
